592393c2491e941ed84fe4d530cb5dc32ff77ae6
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-09-28 18:25:17 +0100

    all

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Python Script """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Python Method """
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'mechken.atef@gmail.com'
+    }
+
+    reddit_req = requests.get("https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after), headers=headers)
+    reddit_info = reddit_req.json().get("data")
+    if reddit_info is None:
+        return None
+    hot_list += reddit_info.get("children", [])
+    after = reddit_info.get("after", None)
+    if after is not None:
+        recurse(subreddit, hot_list, after)
+    return hot_list
